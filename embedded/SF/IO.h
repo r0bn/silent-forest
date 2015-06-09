59a38cdb43d4302bda13a8b45e141fbe2168b65b
Kalbshack <sp.id01@gmx.de>
2015-06-09 17:35:10 +0200

    added comments to IO.h

--- embedded/SF/IO.h
+++ embedded/SF/IO.h
@@ -1,6 +1,9 @@
 #ifndef __IO_H__
 #define __IO_H__
 
+/*
+ * Different modes a LED can performe
+ */
 enum led_mode
 {
     OFF,
@@ -8,12 +11,18 @@ enum led_mode
     PULSE
 };
 
+/*
+ * Different roles
+ */
 enum module_role
 {
     PLAYER,
     HILL
 };
 
+/*
+ * Different status that can be shown on team leds
+ */
 enum team
 {
     TEAM_HILL,
@@ -23,19 +32,41 @@ enum team
 class IO
 {
     private:
+        /*
+         * modes of the differen LED groups (2x status, 2x team)
+         */
         led_mode status_led_1_mode;
         led_mode status_led_2_mode;
         led_mode blue_mode;
         led_mode red_mode;
+        /*
+         * periode LEDs are flashing
+         */
         unsigned int pulse_time = 500;
+        /*
+         * timer for the flashing LEDs
+         */
         unsigned long pulse_timer = 0;
+        /*
+         * defines if LEDs are on or off in PULSE mode
+         */
         bool toggle_leds;
+        /*
+         * defines which status is shown on the team LEDs
+         */
+        team show_team = TEAM_HILL;
 
+        /*
+         * pins of the led and status LEDs
+         */
         byte blue_leds[5] = {A4, A3, A2, A1, A0};
         byte red_leds[5] = {2, 3, 4, 5, 6};
         byte status_led_1_pin = 7;
         byte status_led_2_pin = 8;
-        
+
+        /*
+         * pins of the button(s)
+         */
         byte button_1_pin = A5;
         byte button_2_pin;
 
@@ -43,28 +74,65 @@ class IO
 
 
     public:
+        /*
+         * saves the global/hill status
+         */
         byte team_blue_hill_status;
         byte team_blue_global_status;
         byte team_red_hill_status;
         byte team_red_global_status;
 
+        /*
+         * role of the module
+         */
         module_role role = PLAYER;
 
-        team show_team = TEAM_HILL;
-
+        /*
+         * switching off all LEDs
+         */
         void reset();
+        /*
+         * set the global status for both teams
+         */
         void setGlobalTeam(byte blue, byte red);
+        /*
+         * set the hill status for both teams
+         */
         void setHillTeam(byte blue, byte red);
+        /*
+         * set the mode how the blue, red or both leds will shine (OFF, ON, PUlSE
+         */
         void setModeBlue(led_mode mode);
         void setModeRed(led_mode mode);
         void setModeTeam(led_mode mode);
+        /*
+         * initialize the LEDs (OFF) and buttons
+         */
         void setup();
+        /*
+         * set the mode how the status LEDs will shine (OFF, ON, PULSE)
+         */
         void setStatus1(led_mode mode);
         void setStatus2(led_mode mode);
+        /*
+         * set which status (TEAM_HILL, TEAM_GLOBAL) the team LEDs should show
+         */
         void switchTo(team team_to_switch);
+        /*
+         * update the in- and outputs 
+         */
         void update();
+        /*
+         * update the inputs
+         */
         void updateInputs();
+        /*
+         * update a single LED according to pin_mode (OFF, ON, PULSE)
+         */
         void updateLED(byte pin, led_mode pin_mode);
+        /*
+         * update all LEDs
+         */
         void updateLEDs();
 
 };
